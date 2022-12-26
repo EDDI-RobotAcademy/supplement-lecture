4b36ca88f62bf6eab940537c496e99f966d998a3
silenc3502 <gcccompil3r@gmail.com>
2022-12-26 14:15:53 +0900

    feat: Spring Boot에서 DDD (Domain Driven Development) 맛보기

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.account.entity.Account;
+import com.example.demo.account.vo.Email;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+	@Test
+	void createNewAccountTest () {
+		final int testId = 3;
+		Email email = new Email("test@test.com");
+
+		Account account = new Account(testId, email);
+		System.out.println(account);
+	}
 
 	@Test
 	void contextLoads() {
