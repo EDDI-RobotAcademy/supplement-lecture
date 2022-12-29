536251539937daee695b12314115b967e1099eea
silenc3502 <gcccompil3r@gmail.com>
2022-12-29 12:48:39 +0900

    feat: vue에서 클릭 이벤트 처리하기 [FRONTEND-2]

--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <p>Hello {{ message }}</p>
+        <h2>{{ message.length }}</h2>
+        <h3>{{ list[2] }}</h3>
+        <h4>{{ list[num] }}</h4>
+        <p>{{ buttonClickCount }} 번 클릭했습니다.</p>
+        <button v-on:click="buttonClickIncrementHandler">카운트하기</button>
     </div>
 </template>
 
@@ -10,7 +15,15 @@ export default {
     name: "TestView",
     data () {
         return {
-            message: "이렇게 html과 javascript가 변수화되어 연결이 가능하다."
+            message: "이렇게 html과 javascript가 변수화되어 연결이 가능하다.",
+            list: ['apple', 'banana', 'grape', 'strawberry'],
+            num: 1,
+            buttonClickCount: 0,
+        }
+    },
+    methods: {
+        buttonClickIncrementHandler: function () {
+            this.buttonClickCount += 1
         }
     }
 }
