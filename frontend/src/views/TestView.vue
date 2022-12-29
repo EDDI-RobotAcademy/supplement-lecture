f1811e401adfdc8307cfbdd92934ab8ea65ace3e
silenc3502 <gcccompil3r@gmail.com>
2022-12-29 14:48:30 +0900

    feat: local component 사용 방법 맛보기 [FRONTEND-4]

--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -3,9 +3,9 @@
         <p>Hello {{ message }}</p>
         <h2>{{ message.length }}</h2>
         <h3>{{ list[2] }}</h3>
-        <h4>{{ list[num] }}</h4>
+        <h4>{{ list[number] }}</h4>
         <p>{{ buttonClickCount }} 번 클릭했습니다.</p>
-        <button v-on:click="buttonClickIncrementHandler">카운트하기</button>
+        <button v-on:click="buttonClickIncrementHandler">카운트하기</button><br/>
 
         <!-- GlobalComponent.vue -->
         <!-- 부가적으로 현재 케이스에서 global-component 라는 이름 보다는
@@ -23,7 +23,9 @@
              :initial-test는 실제 global-component가 가지고 있는 정보이며
              이 정보로 전달되는 내용이 현재 컴포넌트의 counter임을 의미합니다.
              사실상 Vue의 시작이자 끝인 부분 - 컴포넌트(Component) -->
-        <global-component :initial-test="counter"/>
+        <global-component :initial-test="counter"/><br/>
+        <local-component :initial-value="num"/><br/>
+        <button @click="plus">클릭해봐!</button>
     </div>
 </template>
 
@@ -31,23 +33,31 @@
 
 import Vue from 'vue'
 import GlobalComponent from '@/components/GlobalComponent.vue'
+import LocalComponent from '@/components/LocalComponent.vue'
 
 Vue.component(GlobalComponent.name, GlobalComponent)
 
 export default {
     name: "TestView",
+    components: {
+        'local-component': LocalComponent
+    },
     data () {
         return {
             message: "이렇게 html과 javascript가 변수화되어 연결이 가능하다.",
             list: ['apple', 'banana', 'grape', 'strawberry'],
-            num: 1,
+            number: 1,
             buttonClickCount: 0,
             counter: 0,
+            num: 0,
         }
     },
     methods: {
         buttonClickIncrementHandler: function () {
             this.buttonClickCount += 1
+        },
+        plus () {
+            this.num++
         }
     }
 }
