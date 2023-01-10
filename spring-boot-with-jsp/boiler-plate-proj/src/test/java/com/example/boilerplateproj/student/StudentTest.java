42b3839306573a7557bab42f905eaf5eee6d6ba8
silenc3502 <gcccompil3r@gmail.com>
2023-01-10 11:23:19 +0900

    feat: 학생 및 수업 엔티티 연동 [JPA-2]

--- spring-boot-with-jsp/boiler-plate-proj/src/test/java/com/example/boilerplateproj/student/StudentTest.java
+++ spring-boot-with-jsp/boiler-plate-proj/src/test/java/com/example/boilerplateproj/student/StudentTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest
 public class StudentTest {
 
@@ -19,12 +22,58 @@ public class StudentTest {
 
     @Test
     void saveStudent () {
-        ClassRoom classRoom = new ClassRoom("기본반");
+        //ClassRoom classRoom = new ClassRoom("기본반");
+        ClassRoom classRoom = new ClassRoom("응용반");
         classRoomRepository.save(classRoom);
 
-        Student student = new Student("크리스티나");
+        //Student student = new Student("크리스티나");
+        //Student student = new Student("제시카");
+        Student student = new Student("마이클");
 
         student.setClassRoom(classRoom);
+        studentRepository.save(student);
+    }
+
+    @Test
+    void findStudent () {
+        Optional<Student> maybeStudent = studentRepository.findById(1L);
+
+        if (maybeStudent.isEmpty()) {
+            System.out.println("찾는 학생이 없습니다!");
+            return;
+        }
+
+        Student student = maybeStudent.get();
+        System.out.println(student);
+    }
+
+    @Test
+    void finaAllStudents () {
+        List<Student> students = studentRepository.findAll();
+        System.out.println(students);
+    }
+    
+    @Test
+    void findStudentWithRoomName () {
+        List<Student> students = studentRepository.findStudentListByRoomName("응용반");
+        System.out.println(students);
+    }
+
+    @Test
+    void modifyStudent () {
+        Optional<Student> maybeStudent = studentRepository.findStudentByRoomName("기본반");
+
+        if (maybeStudent.isEmpty()) {
+            System.out.println("찾는 학생이 없습니다!");
+            return;
+        }
+
+        Student student = maybeStudent.get();
+        ClassRoom newClassRoom = new ClassRoom("새로운 과정");
+
+        classRoomRepository.save(newClassRoom);
+
+        student.setClassRoom(newClassRoom);
         studentRepository.save(student);
     }
 }
