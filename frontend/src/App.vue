81ac975881fed0699f84ca76b8eb8a7ed84455d4
silenc3502 <gcccompil3r@gmail.com>
2023-01-12 12:09:41 +0900

    feat: vuetify initial settings [VUETIFY]

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">HomeView</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/test">Test</router-link> |
       <router-link to="/axiosDataSend2Spring">Vue에서 Spring으로 데이터 전송</router-link> |
