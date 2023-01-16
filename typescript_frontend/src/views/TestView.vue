96d90698804b8734a2b580d5212aceb99d39c76b
silenc3502 <gcccompil3r@gmail.com>
2023-01-16 14:39:52 +0900

    init: How to set Vue + TypeScript + Vuetify

---
+++ typescript_frontend/src/views/TestView.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </v-container>
+  </template>
+  
+  <script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  
+  @Component({
+    components: {
+      HelloWorld,
+    },
+  })
+  export default class HomeView extends Vue {}
+  </script>
+  
